135fd9bb35c94fa3f13b6314b73c389bc1aca31e
cyntachs <edrendacaymat@gmail.com>
2018-08-24 01:26:29 -0400

    Liskel update 2.1.1
    * Added component primaries table
    * Added setPrimary function
    * Refactored graphics and filesystem code
    * Fixed backspace bug

--- liskel2.lua
+++ liskel2.lua
@@ -11,7 +11,7 @@ a lua interpreter if autorun.lua
 does not exist.
 ]]
 _osname = 'Liskel OS'
-_osversion = '2.1'
+_osversion = '2.1.1'
 -- ========== INIT START ========== --
 --+-+-+-+-+ Require +-+-+-+-+--
 function require(pkg)
@@ -19,115 +19,88 @@ function require(pkg)
   return _ENV[pkg] or _G[pkg]
 end
 --+-+-+-+-+ Component +-+-+-+-+--
-setmetatable(component,{__index = function(_,k) return component.getPrimary(k) end})
-fsaddr = component.invoke(component.list("eeprom")(),"getData")
-function component.getPrimary(dev)
-  if dev == 'filesystem' then
-    return component.proxy(fsaddr)
-  end
+setmetatable(component, { __index = function(_, k) return component.getPrimary(k) end })
+fsaddr = component.invoke(component.list("eeprom")(), "getData")
+local _component_primaries = {}
+_component_primaries['filesystem'] = component.proxy(fsaddr)
+function component.setPrimary(dev, addr)
   for k,v in component.list() do
-    if v == dev then
-      return component.proxy(k)
+    if k == addr and v == dev then
+      _component_primaries[dev] = component.proxy(addr)
     end
   end
+end
+function component.getPrimary(dev)
+  if _component_primaries[dev] == null then
+    for k, v in component.list() do
+      if v == dev then component.setPrimary(v,k) break end
+    end
+  end
+  return _component_primaries[dev]
 end
 --+-+-+-+-+ Graphics +-+-+-+-+--
-g = {}
-function g.bind()
-  local screen = component.list('screen')()
-  return component.gpu.bind(screen)
-end
-function g.available()
-  local gpu,screen = component.list('gpu')(),component.list('screen')()
-  return (gpu and screen)
-end
-function g.copy(x,y,w,h,a,b)
-  component.gpu.copy(x,y,w,h,a,b)
-end
-function g.getResolution()
-  return component.gpu.getResolution()
-end
-function g.setResolution(w,h)
-  return component.gpu.setResolution(w,h)
-end
-function g.getDepth()
-  return component.gpu.maxDepth()
-end
-function g.setBG(color)
-  return component.gpu.setBackground(color)
-end
-function g.setFG(color)
-  return component.gpu.setForeground(color)
-end
-function g.fill(x,y,w,h)
-  return component.gpu.fill(x,y,w,h,' ')
-end
-function g.fillc(x,y,w,h,ch)
-  return component.gpu.fill(x,y,w,h,ch)
-end
-function g.drawText(x,y,str)
-  return component.gpu.set(x,y,str)
-end
-local w,h = g.getResolution()
-local cx,cy = (w/2),(h/2)
+g = {
+  bind = function()
+    local screen = component.list('screen')()
+    return component.gpu.bind(screen)
+  end,
+  available = function()
+    local gpu, screen = component.list('gpu')(), component.list('screen')()
+    return (gpu and screen)
+  end,
+  copy = component.gpu.copy,
+  getResolution = component.gpu.getResolution,
+  setResolution = component.gpu.setResolution,
+  getDepth = component.gpu.maxDepth,
+  setBG = component.gpu.setBackground,
+  setFG = component.gpu.setForeground,
+  fill = function(x, y, w, h)
+    return component.gpu.fill(x, y, w, h, ' ')
+  end,
+  fillc = component.gpu.fill,
+  drawText = component.gpu.set,
+}
+local w, h = g.getResolution()
+local cx, cy = (w / 2), (h / 2)
 --+-+-+-+-+ Filesystem +-+-+-+-+--
-f = {}
-f.addr = fsaddr
-function f.setBootAddress(addr)
-  f.addr = addr
-end
-function f.open(file,mode)
-  return component.filesystem.open(file,mode)
-end
-function f.read(handle)
-  return component.filesystem.read(handle,math.huge)
-end
-function f.write(handle,str)
-  return component.filesystem.write(handle,str)
-end
-function f.close(handle)
-  return component.filesystem.close(handle)
-end
-function f.list(dir)
-  return component.filesystem.list(dir or '/')
-end
-function f.mkdir(dir)
-  return component.filesystem.makeDirectory(dir)
-end
-function f.rename(name1,name2)
-  return component.filesystem.rename(name1,name2)
-end
-function f.remove(dir)
-  return component.filesystem.remove(dir)
-end
-function f.readfile(file)
-  local hdl,err = f.open(file,'r')
-  if not hdl then error(err) end
-  local buffer = ''
-  repeat
-    local data, err_read = f.read(hdl)
-    if not data and err_read then error(err_read) end
-    buffer = buffer .. (data or '')
-  until not data
-  f.close(hdl)
-  return buffer
-end
-function f.loadfile(file)
-  return load(f.readfile(file),'='..file)
-end
-function f.runfile(file,argc,args)
-  local prog,err = f.loadfile(file..".lua")
-  if prog then
-    local res = table.pack(xpcall(prog,function(...) return debug.traceback() end,argc,args))
-    if res[1] then
-      return table.unpack(res,2,res.n)
+f = {
+  addr = fsaddr,
+  setBootAddress = function(addr) f.addr = addr end,
+  open = component.filesystem.open,
+  read = function(handle) return component.filesystem.read(handle, math.huge) end,
+  write = component.filesystem.write,
+  close = component.filesystem.close,
+  list = function(dir) return component.filesystem.list(dir or '/') end,
+  mkdir = component.filesystem.makeDirectory,
+  rename = component.filesystem.rename,
+  remove = component.filesystem.remove,
+  readfile = function(file)
+    local hdl, err = f.open(file, 'r')
+    if not hdl then error(err) end
+    local buffer = ''
+    repeat
+      local data, err_read = f.read(hdl)
+      if not data and err_read then error(err_read) end
+      buffer = buffer .. (data or '')
+    until not data
+    f.close(hdl)
+    return buffer
+  end,
+  loadfile = function(file) return load(f.readfile(file), '=' .. file) end,
+  runfile = function(file, argc, args)
+    local prog, err = f.loadfile(file .. ".lua")
+    if prog then
+      local res = table.pack(xpcall(prog, function(...) return debug.traceback() end, argc, args))
+      if res[1] then
+        return table.unpack(res, 2, res.n)
+      else
+        error(res[2])
+      end
     else
-      error(res[2])
+      error(err)
     end
-  else
-    error(err)
-  end
-end
+  end,
+}
 f.load = f.loadfile
 f.run = f.runfile
 --+-+-+-+-+ Error +-+-+-+-+--
@@ -135,32 +108,32 @@ std_error = error
 function error(msg)
   g.setFG(0xFFFFFF)
   g.setBG(0x000000)
-  g.fill(1,1,w,h)
+  g.fill(1, 1, w, h)
   -- write to screen
   local line = 1
   local prev = 1
-  for w in string.gmatch(msg,'()\n') do
-    g.drawText(1,line,msg:sub(prev,w-1))
-    prev = w+1
-    line = line+1
+  for w in string.gmatch(msg, '()\n') do
+    g.drawText(1, line, msg:sub(prev, w - 1))
+    prev = w + 1
+    line = line + 1
   end
-  g.drawText(1,line,msg:sub(prev))
+  g.drawText(1, line, msg:sub(prev))
   -- write to log
-  local hdl,errf = f.open('error_log.txt','w')
+  local hdl, errf = f.open('error_log.txt', 'w')
   if hdl then
-    f.write(hdl,msg..'\n')
+    f.write(hdl, msg .. '\n')
   end
   f.close(hdl)
   repeat
-    local e,addr,char,code = computer.pullSignal()
+    local e, addr, char, code = computer.pullSignal()
   until e == 'key_down' and code == 28
   computer.shutdown(true)
 end
 -- ========== INIT END ========== --
 console = {}
 -- table serialize function
-function console.Serialize(o,depth)
-  local function ser(o,depth,ind)
+function console.Serialize(o, depth)
+  local function ser(o, depth, ind)
     ind = ind or 1
     depth = depth or 3
     depth = depth - 1
@@ -168,7 +141,7 @@ function console.Serialize(o,depth)
     local indent = ''
     while indn < ind do
       indent = indent .. '  '
-      indn =  indn + 1
+      indn = indn + 1
     end
     local retstr = ''
     if type(o) == "number" then
@@ -178,52 +151,52 @@ function console.Serialize(o,depth)
     elseif type(o) == "string" then
       retstr = retstr .. string.format("%q", o) .. ''
     elseif type(o) == "table" then
-      if depth <= 0 then return '<'..type(o)..'>' end
+      if depth <= 0 then return '<' .. type(o) .. '>' end
       retstr = retstr .. '{\n'
-      for k,v in pairs(o) do
+      for k, v in pairs(o) do
         retstr = retstr .. indent .. '' .. k .. '='
-        retstr = retstr .. ser(v, depth, ind+1)
+        retstr = retstr .. ser(v, depth, ind + 1)
         retstr = retstr .. ',\n'
       end
-      retstr = retstr .. indent:sub(1,indent:len()-2) .. '}'
+      retstr = retstr .. indent:sub(1, indent:len() - 2) .. '}'
     else
-      retstr = retstr .. '<'..type(o)..'>'
+      retstr = retstr .. '<' .. type(o) .. '>'
     end
     return retstr
   end
-  return ser(o,depth)
+  return ser(o, depth)
 end
 -- array to string / string to array
 function console.ArrayToStr(tabl)
   local retval = ''
-  for i=1, #tabl do
-    retval = retval ..  tabl[i]
+  for i = 1, #tabl do
+    retval = retval .. tabl[i]
   end
   return retval
 end
 function console.StrToArray(str)
   local retval = {}
-  for i=1, #str do
-    table.insert(retval,str:sub(i,i))
+  for i = 1, #str do
+    table.insert(retval, str:sub(i, i))
   end
   return retval
 end
 -- line output
 function console.lineout(str, line)
-  g.fill(1,line,w,1)
-  g.drawText(1,line,str)
+  g.fill(1, line, w, 1)
+  g.drawText(1, line, str)
 end
-function console.lineoutoff(str,line,offs)
+function console.lineoutoff(str, line, offs)
   offs = offs or 1
-  g.fill(offs,line,w,1)
-  g.drawText(offs,line,str)
+  g.fill(offs, line, w, 1)
+  g.drawText(offs, line, str)
 end
 -- history buffer
 console.history = { --  history system
   mem = {}, -- output lines history
   cmdmem = {}, -- command history
-  size = h*10, -- history size (10 screens)
-  viewheight = h-1, -- height of viewport
+  size = h * 10, -- history size (10 screens)
+  viewheight = h - 1, -- height of viewport
   viewbottom = 1, -- current viewport bottom line
   prevbottom = 1, -- previous view bottom
   recallptr = 1, -- pointer for line recall fuction
@@ -231,17 +204,19 @@ console.history = { --  history system
   lnum = 1, -- line number accumulator
   scrspeed = 5, -- scroll speed
 }
-function console.history.PrintAll() -- reprint viewport
+function console.history.PrintAll()
+  -- reprint viewport
   if next(console.history.mem) == nil then return end
-  for i=1, console.history.viewheight-1 do
-    local bot = console.history.viewbottom - (i-1)
+  for i = 1, console.history.viewheight - 1 do
+    local bot = console.history.viewbottom - (i - 1)
     if bot <= 0 then return end
     local toprint = console.history.mem[bot]
-    local cpos = console.history.viewheight - (i-1)
-    console.lineoutoff(toprint,cpos,console.history.printoffset)
+    local cpos = console.history.viewheight - (i - 1)
+    console.lineoutoff(toprint, cpos, console.history.printoffset)
   end
 end
-function console.history.Update() -- update viewport
+function console.history.Update()
+  -- update viewport
   console.history.PrintAll() -- temporary
 end
 function console.history.ScrollEnd()
@@ -251,7 +226,7 @@ function console.history.ScrollEnd()
 end
 function console.historyScrollTop()
   console.history.prevbottom = console.history.viewbottom
-  console.history.viewbottom = h-2
+  console.history.viewbottom = h - 2
   console.history.PrintAll() -- reprint history
 end
 function console.history.ScrollUp(scr)
@@ -291,15 +266,15 @@ function console.history.Recall()
   return console.history.cmdmem[console.history.recallptr]
 end
 function console.history.Add(str)
-  str = " "..tostring(console.history.lnum).."  | " .. str
-  table.insert(console.history.mem,str)
+  str = " " .. tostring(console.history.lnum) .. "  | " .. str
+  table.insert(console.history.mem, str)
   console.history.viewbottom = #console.history.mem
-  console.history.recallptr = #console.history.cmdmem +1
+  console.history.recallptr = #console.history.cmdmem + 1
   console.history.Update()
   console.history.lnum = console.history.lnum + 1
 end
 function console.history.AddInp(str)
-  table.insert(console.history.cmdmem,str)
+  table.insert(console.history.cmdmem, str)
   console.history.Add(str)
 end
 -- input buffer
@@ -308,29 +283,34 @@ console.input = {
   col = 1, -- current input column
   printoffset = 1, -- print offset
 }
-function console.input.Print() -- print input
+function console.input.Print()
+  -- print input
   local out = console.ArrayToStr(console.input.buffer)
-  console.lineoutoff(out,h,console.input.printoffset)
+  console.lineoutoff(out, h, console.input.printoffset)
 end
 function console.input.SetPrintOffset(offs)
   console.input.printoffset = offs
 end
-function console.input.Append(str) -- append to input buffer
-  table.insert(console.input.buffer,str)
+function console.input.Append(str)
+  -- append to input buffer
+  table.insert(console.input.buffer, str)
   console.input.Print()
 end
-function console.input.Insert(str,pos) -- insert in input buffer
+function console.input.Insert(str, pos)
+  -- insert in input buffer
   pos = pos or console.input.col
-  table.insert(console.input.buffer,pos,str)
+  table.insert(console.input.buffer, pos, str)
   console.input.Print()
 end
-function console.input.SetPos(pos) -- set cursor position
+function console.input.SetPos(pos)
+  -- set cursor position
   pos = pos or #console.input.buffer
   if pos < 1 then pos = 1 end
   if pos > #console.input.buffer then pos = #console.input.buffer + 1 end
   console.input.col = pos
 end
-function console.input.MovePos(mov) -- move cursor position
+function console.input.MovePos(mov)
+  -- move cursor position
   local pos = console.input.col + mov
   console.input.SetPos(pos)
 end
@@ -341,7 +321,7 @@ function console.input.GetString()
   return console.ArrayToStr(console.input.buffer)
 end
 function console.input.DelChar()
-  table.remove(console.input.buffer,console.input.col)
+  table.remove(console.input.buffer, console.input.col)
   console.input.Print()
 end
 function console.input.Clear()
@@ -362,21 +342,21 @@ function console.print(str)
     str = tostring(str)
   end
   local prev = 1
-  for w in string.gmatch(str,'()\n') do
-    console.history.Add(str:sub(prev,w-1))
-    prev = w+1
+  for w in string.gmatch(str, '()\n') do
+    console.history.Add(str:sub(prev, w - 1))
+    prev = w + 1
   end
   console.history.Add(str:sub(prev))
 end
 -- console function
 function console.Run()
   -- screen clear
-  g.fill(1,2,w,h)
+  g.fill(1, 2, w, h)
   -- print os header
   g.setFG(0x000000)
   g.setBG(0xFFFFFF)
-  g.fill(1,1,w,1)
-  g.drawText(1,1,"  ".._osname.." ".._osversion)
+  g.fill(1, 1, w, 1)
+  g.drawText(1, 1, "  " .. _osname .. " " .. _osversion)
   g.setFG(0xFFFFFF)
   g.setBG(0x000000)
   -- console init
@@ -385,7 +365,7 @@ function console.Run()
   local hist = console.history
   local inp = console.input
   print = console.print
-  console.lineout(console_header,h)
+  console.lineout(console_header, h)
   inp.SetPrintOffset(#console_header + 1)
   -- intro text
   print("* F12 to restart  *")
@@ -399,13 +379,13 @@ function console.Run()
       -- command
       if evt[4] == 28 then -- enter key
         hist.AddInp(inp.GetString()) -- add input to history
-        console.lineout(console_header,h)
-        -- parse command
+        console.lineout(console_header, h)
+        -- parse command --
         local luacmd = inp.GetString() -- get string
-        -- execute
-        local loaded,lderr = load(luacmd,'=cinput')
+        -- execute --
+        local loaded, lderr = load(luacmd, '=cinput')
         if loaded then
-          local res,err = xpcall(loaded,function(msg) return msg ..'\n'.. debug.traceback() end)
+          local res, err = xpcall(loaded, function(msg) return msg .. '\n' .. debug.traceback() end)
           if not res and err then
             print(err)
           end
@@ -414,12 +394,14 @@ function console.Run()
         else
           print('Unknown Command')
         end
-        -- done
+        -- done --
         inp.Clear() -- clear input buffer
       elseif evt[4] == 14 then -- backspace
-        inp.MovePos(-1)
-        inp.DelChar()
-        hist.ResetRecall()
+        if inp.col > 1 then
+          inp.MovePos(-1)
+          inp.DelChar()
+          hist.ResetRecall()
+        end
       elseif evt[4] == 200 then -- up key
         hist.MoveRecall(-1)
         inp.SetBuffer(hist.Recall())
@@ -449,7 +431,7 @@ function console.Run()
     if blinkon then -- cursor blink
       local posx = console.input.col + console.input.printoffset - 1
       g.setBG(0xFFFFFF)
-      g.fill(posx,h,1,1)
+      g.fill(posx, h, 1, 1)
       g.setBG(0x000000)
       blinkon = false
     else
@@ -467,7 +449,7 @@ if component.filesystem.exists('autorun.lua') then
   f.runfile('autorun')
 end
 -- run shell
-local res,err = xpcall(main,function(msg) return msg ..'\n'.. debug.traceback() end)
+local res, err = xpcall(main, function(msg) return msg .. '\n' .. debug.traceback() end)
 if not res and err then
   error(err)
 end
